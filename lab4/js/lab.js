be074253ff9cf398873f3ca873d238dec1f4a8ec
Baoqi <byu27@ucsc.edu>
2023-04-23 15:36:53 -0700

    lab4
    first try

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,62 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/**
+ * Author Name: Baoqi Yu & Haien Liang
+ * Date: 04/23/2023
+ * Description: A simple computer game namend Gomoku
+ * Email: <byu27@ucsc.edu>
+/**
 
-// Constants
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//Cooking of Stir-fried eggs and tomatoes
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
+//High-level taskts:
+//1. Find a kitchen or other places for Cooking
+//2. Collect and prepare all tools and ingredients
+   //-tools requirnments: a knife, a spatula, 3 bowls or plates, two pairs of chopsticks, a non-stick pan
+   //-ingredients: tomatoes, eggs, oil. ketchup, salt, green onions
+//3. Cut the tomatoes into small wedges or cubes
+//4. Cut the green onions into small slices
+//5. Beat the eggs in a bowl
+   //-use the chopsticks to stir the eggs
+   //-add a little bit water into the bowl
+//6. Heat a non-stick pan over high heat.
+//7. Add the oil into the non-stick pan
+   //-wait for a few mintues
+//8. Add the eggs into the non-stick pan
+   //-use the spatula to stir-fry until the egg is soft-boiled
+//9. Take out the eggs
+   //-put the eggs into a plate or bowl
+//10. Add the oil again
+   //-wait for a few seconds
+//11. Add the green onions
+   //-use the spatula to stir-fry until the smell comes out
+//12. Add tomatos into the non-stick pan
+//13. Stir-fry green onions and tomatos together for a few mintues 
+//14. Add the eggs into the non-stick pan again
+//15. Add some ketchup and salt 
+//16. Keep using the spatula to stir-fry
+   //-2 mintues in total 
+//17. Turn off the heat and transfer the food to a new serving plate.
+//18. Enjoy the Stir-fried eggs and tomatos 
+//19. Clean the kitchen after eating
 
-// let's get this party started
-main();
+//A simple computer game: gomoku
+//1. Display a game board on the screen
+   //-Draw the grid lines and intersections for the board.
+   //-Label the rows and columns of the board.
+   //-Create a space to display the player's names and scores.
+//2. Allow players to take turns to make a move
+   //-Allow players to undo their last move if they change their mind.
+   //-Implement a turn timer to prevent players from taking too long to make a move
+   //-Ensure that only one stone can be placed on each intersection of the board.
+//3. Validate the move made by the player
+   //-Ensure that the space selected by the player is valid
+   //-Check that the move does not break the game rules 
+//4. Check for a winner after each move
+   //-Check for horizontal, vertical, and diagonal lines of five stones after each move.
+   //-Declare the winner if a line of five stones is found
+   //-Detect if the board is full and the game is a draw.
+//5. Declare the winner or a draw at the end of the game
+//6. Allow players to choose to play another round or exit the game.
+   //-Create a "play again" button to start a new game.
+   //-Create an "exit game" button to close the game.
