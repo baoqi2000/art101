0397db8cbfb2b9780a6532904a09d95f61cfad76
Baoqi <byu27@ucsc.edu>
2023-05-01 16:21:34 -0700

    lab 6
    go!

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,21 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+/*
+ * Author: Baoqi Yu and Haien Liang
+ * Created: 26 April 2023
+ * Email: <byu27@ucsc.edu> <hliang37@ucsc.edu>
+ */ 
+var myTransport = ["airplane", "car", "bus", "subway", "bike", "uber", "spacecraft"]
 
-// Constants
-
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+var myMainRide = {
+  carMake: "Subaru",
+  carModel: "BRZ",
+  carColor: "Black",
+  carYear: "2016",
+  carAge: function() {
+      return 2023 - this.carYear
+  }
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+document.writeln("I want to buy a : ", myTransport, "</br>")
+document.writeln("My Main Ride: <pre>",
+    JSON.stringify(myMainRide, null, '\t'), "</pre>"); 
+    
