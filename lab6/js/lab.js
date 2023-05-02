e8397fbae51f329f3275225dfa16c76639b6e8b9
Baoqi <byu27@ucsc.edu>
2023-05-01 20:17:39 -0700

    lab 6
    giao

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -3,7 +3,7 @@
  * Created: 26 April 2023
  * Email: <byu27@ucsc.edu> <hliang37@ucsc.edu>
  */ 
-var myTransport = ["airplane", "car", "bus", "subway", "bike", "uber", "spacecraft"]
+var myTransport = ["airplane", " car", " bus", " subway", " bike", " uber", " spacecraft"]
 
 var myMainRide = {
   carMake: "Subaru",
@@ -15,7 +15,6 @@ var myMainRide = {
   }
 }
 
-document.writeln("I want to buy a : ", myTransport, "</br>")
+document.writeln("I want to buy a: ", myTransport, "</br>")
 document.writeln("My Main Ride: <pre>",
     JSON.stringify(myMainRide, null, '\t'), "</pre>"); 
-    
