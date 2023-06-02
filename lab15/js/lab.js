839f12c6ce73229664aac4fad7cad64c37e900dc
Baoqi <byu27@ucsc.edu>
2023-06-02 13:27:07 -0700

    asd
    asd

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,34 +1,32 @@
 /*
  * Author: Baoqi Yu & Haien Liang
- * Created: 29 May 2023
+ * Created: 30 May 2023
  * Email: <byu27@ucsc.edu> <hliang37@ucsc.edu>
- * Lab 14: Debugging Tools & Strategies
+ * Lab 15: AJAX
  */ 
 
 // Using the core $.ajax() method
 $.ajax({
-    // The URL for the request (from the api docs)
+    
     url: "https://yesno.wtf/#",
     // The data to send (will be converted to a query string)
     data: { 
-            // here is where any data required by the api 
-            //   goes (check the api docs)
+
             id: 123,
             api_key: "blahblahblah",
           },
-    // Whether this is a POST or GET request
+  
     type: "GET",
-    // The type of data we expect back
+  
     dataType : "json",
-    // What do we do when the api call is successful
-    //   all the action goes in here
+
     success: function(data) {
-        // do stuff
+
         console.log(data);
     },
-    // What we do if the api call fails
+
     error: function (jqXHR, textStatus, errorThrown) { 
-        // do stuff
+
         console.log("Error:", textStatus, errorThrown);
     }
 })
@@ -68,7 +66,6 @@ $(document).ready(function() {
 
 
     $("#get-apod").click(function() {
-      // Make an AJAX request to the NASA APOD API
       $.ajax({
         url: "https://api.nasa.gov/planetary/apod",
         type: "GET",
@@ -77,12 +74,12 @@ $(document).ready(function() {
           api_key: "seGdgn2f4f4NvSuWOhtx2NcMyJwlVuiaylWwsRcd" // Replace with your actual API key
         },
         success: function(data) {
-          // Process and display the APOD data
+     
           var title = data.title;
           var explanation = data.explanation;
           var imageUrl = data.url;
           
-          // Display the APOD information
+ 
           $("#apod-output").html("<h2>" + title + "</h2>");
           $("#apod-output").append("<p>" + explanation + "</p>");
           $("#apod-output").append("<img src='" + imageUrl + "' alt='APOD'>");
